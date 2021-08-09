c27ef10c60d4f3a52719f5baee73b27c6ab77a03
fu.wei <wei.fu@mtime.com>
2021-08-09 11:29:39 +0800

    guava cache test

--- server/src/test/java/rpcfw/server/UtilTest.java
+++ server/src/test/java/rpcfw/server/UtilTest.java
@@ -1,16 +1,35 @@
 package rpcfw.server;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.junit.Test;
 import rpcfw.core.RpcRequest;
 import rpcfw.core.UserService;
+
+import java.util.concurrent.*;
 
 /**
  * Created by fuwei on 2021/8/4.
  */
 public class UtilTest {
+    Cache<String, String> cache = CacheBuilder.newBuilder()
+            .expireAfterWrite(20, TimeUnit.SECONDS)
+            .initialCapacity(50)
+            .softValues()
+            .build();
+
     @Test
-    public void test() {
+    public void test() throws InterruptedException {
+        cache.put("1", "test1");
+        System.out.println(cache.getIfPresent("1"));
+        try {
+            System.out.println(cache.get("2", () -> "test2"));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        TimeUnit.SECONDS.sleep(2L);
+        System.out.println(cache.getIfPresent("1") + " ," + cache.getIfPresent("2"));
         System.out.printf(" >>> "
                 + JSON.toJSONString(new RpcRequest(UserService.class.getName(), "findById", new Object[]{
                 Integer.valueOf(1)
